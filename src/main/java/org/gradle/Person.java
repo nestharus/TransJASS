936bf557626bafa0822a3726ccfbe9c3c2ed76f5
a <mrasolomon@gmail.com>
2015-09-02 16:38:17 -0700

    settings

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -2,15 +2,18 @@ package org.gradle;
 
 import org.apache.commons.collections.list.GrowthList;
 
-public class Person {
-    private final String name;
+public class Person
+{
+	private final String name;
 
-    public Person(String name) {
-        this.name = name;
-        new GrowthList();
-    }
+	public Person(String name)
+	{
+		this.name = name;
+		new GrowthList();
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName()
+	{
+		return name;
+	}
 }
